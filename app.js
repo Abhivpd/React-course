4aa1396b86243f1102f1874cab27439bc2545496
Abhishek <abhishek.shettigar@coditas.com>
2023-07-05 10:22:36 +0530

    episode 2

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const heading1 = React.createElement('h1', {}, 'Heading 1');
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement('h1', {id: 'shruti'}, 'Heading 1');
 const heading2 = React.createElement('h2', {}, 'Heading 2');
 const container = React.createElement(
     'div',
